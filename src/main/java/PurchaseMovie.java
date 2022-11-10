dbe397ce93d4ae4ba7a36b9c20f8b5f685d36a21
Malikosss <malikosss@yandex.ru>
2022-11-10 18:29:15 +0300

    новый класс

---
+++ src/main/java/PurchaseMovie.java
@@ -0,0 +1,9 @@
+public class PurchaseMovie {
+    private int id;
+    private String title;
+
+    public PurchaseMovie(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+}
